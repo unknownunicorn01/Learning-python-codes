100b7a78d8f09a9caf1f067f26340d133cb6a0d4
Shikhar Dutta <149324583+unknownunicorn01@users.noreply.github.com>
2025-11-15 22:18:12 +0530

    Add files via upload

---
+++ a1_python/2practice.py
@@ -0,0 +1,14 @@
+# print("""twikle twinkle little star
+# how i wonder how you are
+# up above the world so high
+# like a dimond in the sky
+# """)
+
+# print(""" if you use tripel qoutes you can write in multilines
+#       ''' like this '''
+#       """)
+
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("oor keyaa haaal hai")
+engine.runAndWait()
